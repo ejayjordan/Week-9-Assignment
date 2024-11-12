f84778a61b229e47332fdfc2b1dc9b94bf89f968
jay <emma.jordan@bsu.edu>
2024-11-12 17:40:38 -0500

    maybe the comma was the problem?

--- src/app/videos/video/delete/page.tsx
+++ src/app/videos/video/delete/page.tsx
@@ -5,12 +5,13 @@ export default async function Page(request : Request) {
     const req = await request.json()
     const prisma = new PrismaClient()
     const res = await prisma.video.delete({
-    where: {
-    id: req.id,
-    }
+        where: {
+            id: req.id
+        }
     })
+
     return(
         Response.json(res)
     )
-    }
+}
     
