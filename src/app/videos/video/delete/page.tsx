626f551170de24ad71267a840bc1985e4c9a4bbe
jay <emma.jordan@bsu.edu>
2024-11-12 17:42:44 -0500

    empty delete page.tsx

--- src/app/videos/video/delete/page.tsx
+++ src/app/videos/video/delete/page.tsx
@@ -1,17 +1,2 @@
-'use server';
-import { PrismaClient } from "@prisma/client";
-
-export default async function Page(request : Request) {
-    const req = await request.json()
-    const prisma = new PrismaClient()
-    const res = await prisma.video.delete({
-        where: {
-            id: req.id
-        }
-    })
-
-    return(
-        Response.json(res)
-    )
-}
+export default function Page(){}
     
