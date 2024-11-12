2acdda5584066e6830ba61a2d0c66a78d4778d1b
jay <emma.jordan@bsu.edu>
2024-11-12 17:33:01 -0500

    addition of default to function

--- src/app/videos/video/delete/page.tsx
+++ src/app/videos/video/delete/page.tsx
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export async function Page(request : Request) {
+export default async function Page(request : Request) {
     const req = await request.json()
     const prisma = new PrismaClient()
     const res = await prisma.video.delete({
