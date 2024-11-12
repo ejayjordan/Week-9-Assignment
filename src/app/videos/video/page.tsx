34488882a7b9f8fbe747ef1e3ac2b8cf30f3cc78
jay <emma.jordan@bsu.edu>
2024-11-12 17:22:53 -0500

    iterator edit

--- src/app/videos/video/page.tsx
+++ src/app/videos/video/page.tsx
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import Link from 'next/link'
 export default async function Page( ) {
     const prisma = new PrismaClient()
 
     const videos = await prisma.video.findMany()
 
-    const videoList = videos.map((video) => 
-        <ul>
-            <li key={"/videos/video/"+video.id}>{video.name}</li>
-            <li key={"/videos/update/"+video.id}>[edit]</li>
-        </ul> 
+    const videoList = videos.map((video, index) =>
+        <li key={index}>
+            <a href={"/videos/video/" + video.id}>{video.name}</a>
+            <a href={"/videos/update/" + video.id}>[edit]</a>
+        </li> 
     )
 
     return( <div>  
