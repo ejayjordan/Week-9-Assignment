d363a549fe4a9e2a96770563f5b0a1ac5b96fba3
jay <emma.jordan@bsu.edu>
2024-11-12 17:13:58 -0500

    addition of key iterator

--- src/app/videos/video/page.tsx
+++ src/app/videos/video/page.tsx
@@ -4,10 +4,10 @@ export default async function Page( ) {
 
     const videos = await prisma.video.findMany()
 
-    const videoList = videos.map((video) => 
+    const videoList = videos.map((video, index) => 
         <li>
-            <a href={"/videos/video/"+video.id}>{video.name}</a>
-            <a href={"/videos/update/"+video.id}>[edit]</a>
+            <a href={"/videos/video/"+video.id} key={index}>{video.name}</a>
+            <a href={"/videos/update/"+video.id} key={index}>[edit]</a>
         </li> 
     )
 
