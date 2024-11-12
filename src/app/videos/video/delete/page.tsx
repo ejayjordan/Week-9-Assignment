b0e7b3d465c0aa38d98791cbdea26dd8ddc65bfd
jay <emma.jordan@bsu.edu>
2024-11-12 17:27:40 -0500

    delete method

---
+++ src/app/videos/video/delete/page.tsx
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function DELETE(request : Request) {
+    const req = await request.json()
+    const prisma = new PrismaClient()
+    const res = await prisma.video.delete({
+    where: {
+    id: req.id,
+    }
+    })
+    return( Response.json(res) )
+    }
+    
