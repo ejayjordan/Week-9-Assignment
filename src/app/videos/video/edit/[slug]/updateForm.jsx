632703915239e7b0147c49a4a42f1508f5cd30ee
jay <emma.jordan@bsu.edu>
2024-11-13 18:22:24 -0500

    the slugs are not working on vercel

--- src/app/videos/video/edit/[slug]/updateForm.jsx
+++ src/app/videos/video/edit/[slug]/updateForm.jsx
@@ -22,7 +22,7 @@ export default function UpdateForm( props ) {
                     type="text" name="runtime" 
                     value={videoLength} onChange={(e) => setVideoLength(e.target.value)} />
             </label>
-            <button type="submit">Update</button>
+            <button type="submit">Edit</button>
         </form>
     )
 }
