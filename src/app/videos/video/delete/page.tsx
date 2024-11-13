804d3607d32937af2c6db42bb81fcc98f447dfb0
jay <emma.jordan@bsu.edu>
2024-11-13 17:33:20 -0500

    addition of deletion page

--- src/app/videos/video/delete/page.tsx
+++ src/app/videos/video/delete/page.tsx
@@ -1,2 +1,15 @@
-export default function Page(){}
+import { PrismaClient } from "@prisma/client";
+import { redirect } from 'next/navigation';
+
     
+
+export default async function Page( {params}: { params: { slug: number } }) {
+    'use server';
+    const prisma = new PrismaClient()
+    await prisma.video.delete({
+        where: {
+            id: Number(params.slug)
+        }
+    })
+    redirect('/videos')
+}
